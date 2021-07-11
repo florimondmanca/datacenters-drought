d4fa0dbf9b63871cd08dd011bc77cd2e7c0cc837
florimondmanca <florimond.manca@protonmail.com>
2021-07-11 19:44:59 +0200

    Beautify plot

--- dcd/mapgen.py
+++ dcd/mapgen.py
@@ -5,30 +5,27 @@ import geoplot.crs as gcrs
 
 from . import datasrc
 
-# 1) Load datacenter data.
-datacenters_west = datasrc.get_datacenters_west()
-
-# 2) Draw USA West basemap.
+# Draw USA West basemap.
 contiguous_us_west = datasrc.get_contiguous_us_west()
+extent = contiguous_us_west.total_bounds  # Used to fit other plots.
 ax = gplt.polyplot(
     contiguous_us_west,
     projection=gcrs.AlbersEqualArea(),
     linewidth=0.5,
+    edgecolor="gray",
     zorder=1,
 )
 
-# 3) Overlay drought data.
-drought_us_west = datasrc.get_drought_us_west()
-
-# Draw drought areas colored by drought intensity.
+# Overlay drought areas, colored by intensity.
 # See: https://residentmario.github.io/geoplot/plot_references/plot_reference.html#choropleth
+drought_us_west = datasrc.get_drought_us_west()
 gplt.choropleth(
     drought_us_west,
     hue="DM",
-    cmap="YlOrRd",  # Light yellow to dark red
+    cmap="YlOrRd",
     legend=True,
     scheme=mc.FisherJenks(drought_us_west["DM"], k=5),  # 5-category legend
-    legend_kwargs={"bbox_to_anchor": (1, 0.35)},
+    legend_kwargs={"bbox_to_anchor": (0.8, 0.7)},
     legend_labels=[
         "D0 (Abnormally Dry)",
         "D1 (Moderate Drought)",
@@ -36,15 +33,22 @@ gplt.choropleth(
         "D3 (Extreme Drought)",
         "D4 (Exceptional Drought)",
     ],
-    ax=ax,
     zorder=0,
+    ax=ax,
+    extent=extent,
 )
 
-# 4) Overlay datacenters in US West on map.
+# Overlay datacenters in US West on map.
+datacenters_west = datasrc.get_datacenters_west()
 gplt.pointplot(
     datacenters_west,
-    ax=ax,
     zorder=2,
+    linewidth=0.8,
+    s=5,
+    color="white",
+    edgecolor="black",
+    ax=ax,
+    extent=extent,
 )
 
 plt.savefig("out/result.svg")
